bd25c1103495bbbac423dffabc9595e9a3e4bd65
Yujun Qin <qinyujun@umich.edu>
2020-07-28 23:16:28 +0800

    temp final

---
+++ final/bearing.py
@@ -0,0 +1,153 @@
+import scipy.io
+import scipy.signal as ss
+import numpy as np
+import matplotlib.pyplot as plt
+import fft_process
+import wavelet_threshold_denoise as wtd
+import sys
+import os
+from parameter_tune import execute_out as Tune
+from parameter_tune import Tune_lite
+from parameter_tune import Tune_denoise
+# from TQWT_std import tqwt
+# from ITQWT_std import itqwt
+from tqwt_tools import tqwt, itqwt
+
+def get_slice(dat, index):
+    '''
+    retrieve the i-th block in original files
+    '''
+    return dat[index * 20480: (index + 1) * 20480]
+
+def format_n(i):
+    return '{:0>4d}'.format(i)
+
+
+def find_freq(freqs):
+    '''
+    从突出频率中选取原频率及二倍频同时存在的频率
+    '''
+    ret = []
+    for f in freqs:
+        if np.min(np.abs(np.array(freqs) - 2 * f)) < 2:
+            ret.append(f)
+    return ret
+
+def extract_signal(freq, dat):
+    '''
+    从频谱中提取突出信号，提取幅值大于周围信号平均值 * multiplier的信号
+    '''
+    width = 5
+    half_width = width // 2
+    multiplier = 2.5
+    l = []
+    thresholds = dat[:]
+    tSum = np.sum(dat[:width])
+    for i in range(half_width, len(dat) - half_width - 1):
+        thresholds[i] = (tSum - dat[i]) * multiplier / (width - 1)
+        tSum = tSum - dat[i - half_width] + dat[i + half_width + 1]
+    for i in range(half_width, len(dat) - half_width - 1):
+        if freq[i] >= 5000:
+            break
+        if dat[i] > thresholds[i]:
+            l.append(freq[i])
+        
+    
+    return find_freq(l) # 提取出可能为特征频率的值
+    # if len(l) > 1 and np.abs(l[1] - 2 * l[0]) < 2:
+    #     return l[0]
+    # else:
+    #     return None
+
+
+def diagnose(s, b, a, fft_model, plot=True):
+    '''
+    诊断主函数
+    输入：s: 振动信号
+          b, a: 滤波器参数 (scipy.signal.butter)
+          fft_model: 用于计算fft的模型 (fft_process)
+    输出：特征频率
+    '''
+    # q, r, l = Tune_lite(s[:5000]) # TQWT参数调优
+    # w = tqwt(s, q, r, l)
+    # recomposed = itqwt(w[:-1], q, r, len(s)).real
+    # plt.subplot(2, 1, 1)
+    # plt.plot(s)
+    # plt.subplot(2, 1, 2)
+    # plt.plot(recomposed)
+    # plt.show()
+    recomposed = s
+    level, wavelet, mode, substitute, method = Tune_denoise(recomposed) # 选择最优降噪参数
+    wtdm = wtd.WaveletThresholdDenoise({'level': level, 'wavelet': wavelet, 'mode': mode, 'substitute': substitute, 'method': method})
+    denoised = wtdm.wavelet_denoise([recomposed])[0] # 降噪
+    filtered = ss.filtfilt(b, a, denoised) # 滤波
+    hil = ss.hilbert(filtered) # hilbert 变换
+    envelope = np.sqrt(np.power(filtered, 2) + np.power(hil, 2)) #计算包络
+    fft_out = fft_model.fft_analysis(envelope) # 计算频谱
+    if plot:
+        plt.plot(fft_out[1][:2000], fft_out[0][:2000])
+    return extract_signal(fft_out[1], fft_out[0]) # 从频谱提取故障频率
+
+def freq_to_fault(freqs, rpm = 1, ratio = {'outer': 300, 'inner': 210, 'roller': 130}):
+    '''
+    将故障频率转译为故障位置
+    '''
+    ret = []
+    for k in ratio.keys():
+        for f in freqs:
+            if np.abs(f / (ratio[k] * rpm) - 1) < 0.02 or np.abs(f / (2 * ratio[k] * rpm) - 1) < 0.02 or np.abs(f / (3 * ratio[k] * rpm) - 1) < 0.02:
+                ret.append(k)
+                break
+    return ret
+
+if __name__ == '__main__':
+    # use '1', '2', '3' to get the three data sets
+    # the next index is the bearing index
+
+    fft_model = fft_process.FFTProcess({"fs": 20480})
+
+    para=dict()
+    para['level']=2
+    para['wavelet']='db1'
+    para['mode']='less'# soft hard greater less garrot
+    para['substitute']=0
+    para['method']='minimaxi'# sqtwolog rigsure heursure minimaxi
+    wtdm = wtd.WaveletThresholdDenoise(para)
+
+
+
+    datasets = ['1']
+    b, a = ss.butter(4, 0.11, 'highpass')
+    n = 10
+
+    old_params = None
+    pfile = open("py/params.txt")
+    pstr = pfile.read().split('\n')
+    if pstr[-1] == '':
+        del pstr[-1]
+    amps = np.float64(pstr)
+    pfile.close()
+    print(amps)
+    old_params = amps
+    s = simulate.simulate(20480, 2000, 50, 5000, [300, 210, 130], 50, amps, 0.3)
+    cha_f = diagnose(s, b, a, fft_model, plot=True)
+    faults = freq_to_fault(cha_f)
+    fft_out = fft_model.fft_analysis(s)
+    # plt.plot(fft_out[1], fft_out[0])
+    plt.savefig('py/spectrum.png')
+    plt.clf()
+    print(cha_f)
+    print(faults)
+    while True:
+        try:
+            result = open('py/result.txt', 'w')
+            break
+        except:
+            continue
+    for f in ['outer', 'inner', 'roller']:
+        if f in faults:
+            result.write('1')
+        else:
+            result.write('0')
+
+    result.close()     
---
+++ final/parameter_tune.py
@@ -0,0 +1,304 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from scipy import stats as stats
+import scipy.signal as ss
+# from TQWT_std import tqwt as tqwt
+# from ITQWT_std import itqwt as itqwt
+from tqwt_tools import tqwt, itqwt
+import pdb
+import scipy.io
+import matplotlib.pyplot as plt
+import fft_process
+import wavelet_threshold_denoise as wtd
+import math
+
+
+def get_slice(dat, index):
+    '''
+    retrieve the i-th block in original files.
+    '''
+    return dat[index * 20480: (index + 1) * 20480]
+
+
+def getEntropy(y):
+    '''
+    Return the envelop Entrop of input signal used for IK calculation.
+    '''
+    fft_model = fft_process.FFTProcess({"fs": 20480}) # choose fft_model
+    para=dict()
+    para['level']=2
+    para['wavelet']='db1'
+    para['mode']='less'# soft hard greater less garrot
+    para['substitute']=0
+    para['method']='minimaxi'# sqtwolog rigsure heursure minimaxi
+    wtdm = wtd.WaveletThresholdDenoise(para) # choose wavelet_threshold denosie model
+
+    b, a = ss.butter(4, 0.1, 'highpass')
+    denoised = y # wtdm.wavelet_denoise([y])[0]
+    filtered = ss.filtfilt(b, a, denoised)
+    hil = ss.hilbert(filtered)
+    envelope = np.sqrt(np.power(filtered, 2) + np.power(hil, 2))
+    fft_out = fft_model.fft_analysis(envelope)[0]
+    prob = np.array(fft_out)*1.0/(sum(fft_out))
+    entropy = 0
+    for p in prob:
+        entropy += -p*np.log(p) 
+    return entropy
+
+
+def find_kurtosis(data, Q, red,level_num):
+    '''
+    Find a K or IK for a given signal
+    '''
+    w = tqwt(data, Q, red, level_num)
+    comb_signals = []
+
+    #########################################
+    for j in range (level_num + 1):
+        comb_signal = [w[i].real for i in range(j+1)]
+        comb_signals.append(comb_signal)
+    for j in range (level_num):
+        comb_signals.append([np.array(w[j].real)]) # append the decomposed signals w[i] on the end of the []
+
+    #######################################
+    max_kurtosis = 0
+    chosen_level = 0
+    i=0
+    re_signals = []
+    for comb_signal in comb_signals:
+        i = i + 1
+        y = itqwt(comb_signal, Q, red, len(data))
+        re_signals.append(y)
+        kurtosis = stats.kurtosis(y).real
+        ########################################
+        # Comment out to introduce the IK value
+        E = getEntropy(y)
+        kurtosis = kurtosis*1.0 / E # Now the kurtosis is IK
+        #########################################
+        if kurtosis > max_kurtosis:
+            max_kurtosis = kurtosis.real
+            chosen_level = i # index starts from 1
+
+    return max_kurtosis, chosen_level, comb_signals[chosen_level-1]
+
+
+def Tuning_q(data, num, r, l):
+    '''
+    Tune Q value given fixed r, l.
+    '''
+    gap = 0.1
+    Qs = [1.0+i*gap for i in range(num)]
+    reds = [r for i in range(num)]
+    levels = [l for i in range(num)]
+
+    signal_candi = []
+    k_vals = []
+    levels_candi = []
+    for i in range(num):
+        q = Qs[i]
+        red = reds[i]
+        level_num = levels[i]
+        try:
+            max_k, level, sub_band = find_kurtosis(data, q, red, level_num)
+        except:
+            continue
+
+        signal_candi.append(sub_band)
+        k_vals.append(max_k)
+        levels_candi.append(level)
+
+    maxx_k = max(k_vals)
+    index = k_vals.index(maxx_k)
+
+    return Qs[index]
+
+
+def Tuning_r(data, num, q, l):
+    '''
+    Tune r value given fixed q, l.
+    '''
+    gap = 0.1
+    Qs = [q for i in range(num)]
+    reds = [3.0 + 0.1*gap for i in range(num)]
+    levels = [l for i in range(num)]
+
+    signal_candi = []
+    k_vals = []
+    levels_candi = []
+    for i in range(num):
+        q = Qs[i]
+        red = reds[i]
+        level_num = levels[i]
+        try:
+            max_k, level, sub_band = find_kurtosis(data, q, red, level_num)
+        except:
+            continue
+
+        signal_candi.append(sub_band)
+        k_vals.append(max_k)
+        levels_candi.append(level)
+    try:
+        maxx_k = max(k_vals)
+    except:
+        return 3.0
+    index = k_vals.index(maxx_k)
+
+    return reds[index]
+
+
+def Tuning_l(data, num, q, r):
+    '''
+    Tune l value given fixed q, r.
+    '''
+    gap = 1
+    Qs = [q for i in range(num)]
+    reds = [r for i in range(num)]
+    levels = [1 + gap*i for i in range(num)]
+
+    signal_candi = []
+    k_vals = []
+    levels_candi = []
+    for i in range(num):
+        q = Qs[i]
+        red = reds[i]
+        level_num = levels[i]
+        max_k, level, sub_band = find_kurtosis(data, q, red, level_num)
+        try:
+            pass
+        except:
+            continue
+
+        signal_candi.append(sub_band)
+        k_vals.append(max_k)
+        levels_candi.append(level)
+
+    maxx_k = max(k_vals)
+    index = k_vals.index(maxx_k)
+
+    return levels[index]
+
+
+def Tune_lite(data):
+    '''
+    Lite algorithm of finding the optimal parameteric settings.
+    '''
+    q_num = 15
+    r_num = 10
+    l_num = 8
+    q_init = 1.0
+    r_init = 3.0
+
+    l_optimal = Tuning_l(data, l_num, q_init, r_init)
+    q_optimal = Tuning_q(data, q_num, r_init, l_optimal)
+    r_optimal = Tuning_r(data, r_num, q_optimal, l_optimal)
+    # print(1)
+    return q_optimal, r_optimal, l_optimal
+
+def Tune_denoise(data):
+    '''
+    降噪调优主函数：爬山算法
+    一次替换一个参数使得IK值提升，直至IK值无法继续提升，达到局部最优
+    '''
+    level_range = range(5, 20)
+    wavelet_range = ['db1', 'db2', 'db3', 'db4', 'db5']
+    mode_range = ['soft', 'hard', 'greater', 'less', 'garotte']
+    substitute_range = range(5)
+    method_range = ['minimaxi'] #['sqtwolog', 'heursure', 'minimaxi']
+
+    method = 'minimaxi'
+    mode = 'hard'
+    level = 7
+    wavelet = 'db1'
+    substitute = 0
+    best_k = 0
+    while True:
+        method, _ = Tuning_denoise_single(data, 'method', method_range, method=method, mode=mode, level=level, wavelet=wavelet, substitute=substitute)
+        mode, _ = Tuning_denoise_single(data, 'mode', mode_range, method=method, mode=mode, level=level, wavelet=wavelet, substitute=substitute)
+        level, _ = Tuning_denoise_single(data, 'level', level_range, method=method, mode=mode, level=level, wavelet=wavelet, substitute=substitute)
+        wavelet, _ = Tuning_denoise_single(data, 'wavelet', wavelet_range, method=method, mode=mode, level=level, wavelet=wavelet, substitute=substitute)
+        substitute, bk = Tuning_denoise_single(data, 'substitute', substitute_range, method=method, mode=mode, level=level, wavelet=wavelet, substitute=substitute)
+        if bk == best_k:
+            break
+        else:
+            best_k = bk
+    return level, wavelet, mode, substitute, method
+
+
+def Tuning_denoise_single(data, op_name, options, level = 2, wavelet = 'db1', mode = 'less', substitute = 0, method = 'minimaxi'):
+    '''
+    降噪调优子函数：固定其他参数，对于某一参数找到其最大值
+    '''
+    best_k = -100
+    best_option = None
+    for c in options:
+        params = {'level': level, 'wavelet': wavelet, 'mode': mode, 'substitute': substitute, 'method': method}
+        params[op_name] = c
+        wtdm = wtd.WaveletThresholdDenoise(params)
+        dn = wtdm.wavelet_denoise([data])[0]
+        kurtosis = stats.kurtosis(dn).real / getEntropy(dn)
+        if kurtosis > best_k:
+            best_k = kurtosis
+            best_option = c
+
+    return best_option, best_k
+
+
+def execute_out(data_raw):
+    '''
+    Input: signal of a certain channel
+    Output: optimal parameters (Q, r, L) for the channel signals
+    '''
+    data_list = []
+    datalen = data_raw.shape[0] // 20480
+    for i in range(datalen - 200, datalen - 150):#data20.shape[0] // 20480):
+        slice_n = list(get_slice(data_raw, i))
+        data_list.append(slice_n)
+    
+    Qs = []
+    Rs = []
+    Ls = []
+    ##################################
+    for data in data_list:
+        data = np.array(data)
+        q, r, l = Tune_lite(data)
+        Ls.append(l)
+        Qs.append(q)
+        Rs.append(r)
+    
+    #####################################
+    # Analysis
+
+    # K_filtered = np.array([value for i, value in enumerate(Ks) if value > 3])
+    # indexs = [i for i, value in enumerate(Ks) if value > 3]
+    # Q_filtered = np.array([Qs[i] for i in indexs])
+
+    R_opt = np.mean(Rs)
+    Q_opt = np.mean(Qs)
+    L_opt = int(np.mean(Ls))
+    # print(Ls)
+
+    # print(Q_opt, R_opt, L_opt)
+    return Q_opt, R_opt, L_opt
+
+
+
+if __name__ == "__main__":
+    pdb.set_trace()
+    data = scipy.io.loadmat('./preprocess_data/preprocessed/bearing.mat')
+    # use '1', '2', '3' to get the three data sets
+    # the next index is the bearing index
+
+    data20 = data['2'][0] # gets the first bearing in dataset 2
+    # data12 = data['1'][2]
+    # data13 = data['1'][3]
+    # data32 = data['3'][2]
+
+    # execute(data20, 'data20') # dataset 2, bearing 1
+    # execute(data12, 'data12') # dataset 1, bearing 3
+    # execute(data13, 'data13') # dataset 1, bearing 4
+    # execute(data32, 'data32') # dataset 3, bearing 3
+
+    execute_out(data20)
+
